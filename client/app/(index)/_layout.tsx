caaf4c7a2e3464b9d336226aa83699091a680d5f
Dimitar Sabev <dimitarsabev00@gmail.com>
2025-02-16 17:06:00 +0200

    Update AppIndexLayout

--- client/app/(index)/_layout.tsx
+++ client/app/(index)/_layout.tsx
@@ -1,13 +1,66 @@
-import { View, Text } from "react-native";
 import React from "react";
-import { Stack } from "expo-router";
+import { useNetworkState } from "expo-network";
+import { Redirect, Stack } from "expo-router";
+import { Alert } from "react-native";
+import { SignedIn, useUser } from "@clerk/clerk-expo";
 
-const HomeRoutesLayout = () => {
+export default function AppIndexLayout() {
+  const { user } = useUser();
+  const networkState = useNetworkState();
+
+  React.useEffect(() => {
+    if (
+      !networkState.isConnected &&
+      networkState.isInternetReachable === false
+    ) {
+      Alert.alert(
+        "🔌 You are offline",
+        "You can keep using the app! Your changes will be saved locally and synced when you are back online."
+      );
+    }
+  }, [networkState.isConnected, networkState.isInternetReachable]);
+
+  if (!user) {
+    return <Redirect href="/(auth)" />;
+  }
+
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <SignedIn>
+      <Stack
+        screenOptions={{
+          ...(process.env.EXPO_OS !== "ios"
+            ? {}
+            : {
+                headerLargeTitle: true,
+                headerTransparent: true,
+                headerBlurEffect: "systemChromeMaterial",
+                headerLargeTitleShadowVisible: false,
+                headerShadowVisible: true,
+                headerLargeStyle: {
+                  // NEW: Make the large title transparent to match the background.
+                  backgroundColor: "transparent",
+                },
+              }),
+        }}
+      >
+        <Stack.Screen
+          name="list/new/index"
+          options={{
+            presentation: "formSheet",
+            sheetGrabberVisible: true,
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="profile"
+          options={{
+            presentation: "formSheet",
+            sheetAllowedDetents: [0.75, 1],
+            sheetGrabberVisible: true,
+            headerShown: false,
+          }}
+        />
+      </Stack>
+    </SignedIn>
   );
-};
-
-export default HomeRoutesLayout;
+}
