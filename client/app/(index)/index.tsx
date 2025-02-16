6fadacbc720d19315ea09ce9178239ddb806b850
Dimitar Sabev <dimitarsabev00@gmail.com>
2025-02-16 11:08:41 +0200

    Added Header For HomeScreen

--- client/app/(index)/index.tsx
+++ client/app/(index)/index.tsx
@@ -1,13 +1,55 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, Platform, StyleSheet } from "react-native";
 import React from "react";
-import { ThemedText } from "@/components/ThemedText";
+import { Stack } from "expo-router";
+import { IconSymbol } from "@/components/ui/IconSymbol";
+
+const ICON_COLOR = "#007AFF";
 
 const HomeScreen = () => {
+  const renderHeaderRight = () => (
+    <Pressable
+      // onPress={handleNewListPress}
+      onPress={() => {}}
+      style={styles.headerButton}
+    >
+      <IconSymbol name="plus" color={ICON_COLOR} />
+    </Pressable>
+  );
+
+  const renderHeaderLeft = () => (
+    <Pressable
+      // onPress={handleProfilePress}
+      onPress={() => {}}
+      style={[styles.headerButton, styles.headerButtonLeft]}
+    >
+      <IconSymbol
+        name="gear"
+        color={ICON_COLOR}
+        style={{ marginRight: Platform.select({ default: 0, android: 8 }) }}
+      />
+    </Pressable>
+  );
+
   return (
-    <View>
-      <ThemedText type="title">HomeScreen</ThemedText>
-    </View>
+    <>
+      <Stack.Screen
+        options={{
+          title: "Shopping lists",
+          headerRight: renderHeaderRight,
+          headerLeft: renderHeaderLeft,
+        }}
+      />
+    </>
   );
 };
-
+const styles = StyleSheet.create({
+  headerButton: {
+    padding: 8,
+    paddingRight: 0,
+    marginHorizontal: Platform.select({ web: 16, default: 0 }),
+  },
+  headerButtonLeft: {
+    paddingLeft: 0,
+  },
+});
 export default HomeScreen;
