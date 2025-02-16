49eef084b40bda9f756985ac47279ec507738d10
Dimitar Sabev <dimitarsabev00@gmail.com>
2025-02-16 10:48:52 +0200

    Added SignIn, SignUp & ResetPassword Screens For Auth Flow

--- client/app/(auth)/_layout.tsx
+++ client/app/(auth)/_layout.tsx
@@ -1,13 +1,37 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { Stack } from "expo-router";
+import { Redirect, Stack } from "expo-router";
+import { useAuth } from "@clerk/clerk-expo";
 
-const AuthRoutesLayout = () => {
+export default function AuthRoutesLayout() {
+  const { isLoaded, isSignedIn } = useAuth();
+
+  if (!isLoaded) return null;
+
+  if (isSignedIn) return <Redirect href="/(index)" />;
+
   return (
-    <Stack>
-      <Stack.Screen name="index" />
+    <Stack
+      screenOptions={{
+        ...(process.env.EXPO_OS !== "ios"
+          ? {}
+          : {
+              headerLargeTitle: true,
+              headerTransparent: true,
+              headerBlurEffect: "systemChromeMaterial",
+              headerLargeTitleShadowVisible: false,
+              headerShadowVisible: true,
+              headerLargeStyle: {
+                // NEW: Make the large title transparent to match the background.
+                backgroundColor: "transparent",
+              },
+            }),
+      }}
+    >
+      <Stack.Screen name="index" options={{ headerTitle: "Sign in" }} />
+      <Stack.Screen name="sign-up" options={{ headerTitle: "Sign up" }} />
+      <Stack.Screen
+        name="reset-password"
+        options={{ headerTitle: "Reset password" }}
+      />
     </Stack>
   );
-};
-
-export default AuthRoutesLayout;
+}
